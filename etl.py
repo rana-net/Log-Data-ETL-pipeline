77a34970dac40e33399be37e4c2f317a9166a8f7
Rana Roy <ranaroy4650@gmail.com>
2026-05-03 17:06:33 +0530

    Update etl.py

--- etl.py
+++ etl.py
@@ -3,15 +3,11 @@ import sqlite3
 
 print("Starting ETL Pipeline...")
 
-# ======================
-# STEP 1: EXTRACT
-# ======================
+
 df = pd.read_csv("logs.txt")
 print("Data Loaded:\n", df)
 
-# ======================
-# STEP 2: TRANSFORM
-# ======================
+
 df['cpu_usage'] = df['cpu_usage'].astype(int)
 
 # Create new column (important for interviews)
@@ -19,9 +15,7 @@ df['status_flag'] = df['status'].apply(lambda x: 1 if x == "FAIL" else 0)
 
 print("\nTransformed Data:\n", df)
 
-# ======================
-# STEP 3: LOAD
-# ======================    
+
 conn = sqlite3.connect("network_data.db")
 df.to_sql("logs", conn, if_exists="replace", index=False)
 
